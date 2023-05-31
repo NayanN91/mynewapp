a3a70034517d2b214fa89be0bb5567e6f31cdf54
NayanN91 <nayan.nanouche@gmail.com>
2023-05-31 11:08:20 +0200

    ajoutCondition

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,10 +1,30 @@
-function PokemonCard() {
-    return(
+const pokemonList = [
 
-   <figure> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" /> </figure>
-    );
+    {
   
+      name: "bulbasaur",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
+    },
+  
+    {
+  
+      name: "mew",
+  
+    },
+  
+  ];
+
+
+  function PokemonCard() {
+    const pokemon = pokemonList[1];
+  
+    return <p>{pokemon === pokemonList[0] ? <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" /> : "???"}</p>
   }
   
-  
   export default PokemonCard;
+
+ 
