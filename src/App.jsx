e7b5ab070d6b367c913d84ce1214060e2b213e1b
NayanN91 <nayan.nanouche@gmail.com>
2023-06-01 10:33:44 +0200

    src/App.jsx     src/PokemonCard.jsx    src/components/PokemonCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 
-import PokemonCard from "./PokemonCard";
 
 
-function App() {
+import PokemonCard from "./components/PokemonCard.jsx"
 
+
+
+const pokemonList = [
+
+  {
+
+    name: "bulbasaur",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+  },
+
+  {
+
+    name: "mew",
+
+  },
+
+];
+
+function App () {
   return (
 
-    <div>
+    <>
+      <PokemonCard image = {pokemonList[0].imgSrc} name = {pokemonList[0].name}
+       />
+    </>
 
-      <PokemonCard/>
-
-      </div>
-  );
-
+  )
 }
 
 export default App;
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+
+function PokemonCard ({pokemon}) {
+  console.log(pokemon);
+
+  return (
+    <figure>
+      {pokemon.imgSrc != null ? ( <img src={pokemon.imgSrc} alt={pokemon.name} /> ) : (<p>???</p>)}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
